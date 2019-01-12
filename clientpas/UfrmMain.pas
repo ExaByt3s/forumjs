1ae4d60fedca53e755292e089d386420ad4538b0
William <skilltrai@gmail.com>
2019-01-12 18:12:29 -0400

    Agregando clientes

---
+++ clientpas/UfrmMain.pas
@@ -0,0 +1,25 @@
+unit UfrmMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts;
+
+type
+  TfrmMain = class(TForm)
+    lyMainLayout: TLayout;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
