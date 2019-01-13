0f02fdd2c0272301d900646c8258798d4d5adae1
William <skilltrai@gmail.com>
2019-01-12 22:53:49 -0400

    Orgranizando el proyecto RAD STUDIO

--- clientpas/UfrmMain.pas
+++ clientpas/UfrmMain.pas
@@ -4,11 +4,23 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.TabControl, FMX.Edit, FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TfrmMain = class(TForm)
     lyMainLayout: TLayout;
+    tbMain: TTabControl;
+    tbiLogin: TTabItem;
+    tbiApplication: TTabItem;
+    lyLeft: TLayout;
+    lyRight: TLayout;
+    LangGlobal: TLang;
+    lyContentLog: TLayout;
+    btnStartSession: TButton;
+    txtUsername: TEdit;
+    txtPassword: TEdit;
+    lblSignUp: TLabel;
   private
     { Private declarations }
   public
---
+++ clientpas/controller/UDataModule.dfm
@@ -0,0 +1,7 @@
+object dmData: TdmData
+  OldCreateOrder = False
+  OnCreate = DataModuleCreate
+  OnDestroy = DataModuleDestroy
+  Height = 260
+  Width = 351
+end
